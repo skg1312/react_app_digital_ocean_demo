10d5da9f95af9ccfc84c1d417ec1d2eac673465e
Saikumar guduru <skg.13.edu@gmail.com>
2023-10-21 14:46:27 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import QRCode from "qrcode.react";
 
-function App() {
+const App = () => {
+  const qrCodeValue = "https://saikumarguduru.me";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1>QR Code Generator</h1>
+      <div>
+        <QRCode value={qrCodeValue} size={256} fgColor="#000" />
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
